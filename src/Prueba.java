8316d4cae1bbb014f94c966aaeb58afa01fafd84
Blacksadx <jhonblacksad@gmail.com>
2022-08-01 16:37:12 -0500

    Prueba 2

---
+++ src/Prueba.java
@@ -0,0 +1,12 @@
+public class Prueba {
+    public static void main (String args []){
+        int a=10;
+        int b=5;
+        String name= "Buena ";
+
+        System.out.println(a+b);
+        System.out.println(name + "Prueba");
+
+    }
+
+}
